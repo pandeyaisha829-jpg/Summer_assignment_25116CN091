d50f1196788991b9e03ec12618bf8ca0a13b0bad
Aisha <pandeyaisha829@gmail.com>
2026-05-23 18:26:29 +0530

    second program of Day-2

---
+++ Day-2/program2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//reverse of a number
+int main () {
+    int n,i,reverse=0,remainder;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    while(n>0){
+    remainder=n%10;
+    reverse=reverse*10+remainder;
+    n=n/10;
+    }
+    printf("reverse is %d",reverse);
+}
