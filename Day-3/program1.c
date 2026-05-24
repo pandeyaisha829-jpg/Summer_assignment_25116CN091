4528e676ba977f47d76901fd4446d673d4bc8787
Aisha <pandeyaisha829@gmail.com>
2026-05-24 20:54:22 +0530

    first program of Day-3

---
+++ Day-3/program1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//check the number is prime or not
+int main () {
+    int n,i,a;
+    printf("enter n");
+    scanf("%d",&n);
+    a=0;
+    for(i=2; i<=n/2; i++){
+        if(n%i==0){
+        a=1;
+        break;
+        }
+    }
+    if(a==0){
+        printf("prime");
+    }
+    else{
+        printf("not prime");
+    }
+    return 0;
+    
+}
