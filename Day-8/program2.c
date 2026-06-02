271e3c2d64f51955d269a50ca361ef6db576fc7d
Aisha <pandeyaisha829@gmail.com>
2026-06-02 17:28:57 +0530

    2nd program of Day-8

--- Day-8/program2.c
+++ Day-8/program2.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-//print half pyramid pattern
+//print number triangle
 int main () {
     int row,i,j;
     printf("enter the number of rows");
     scanf("%d",&row);
     for(i=1; i<=row; i++){
         for(j=1; j<=i; j++){
-            printf("* ");
+            printf("%d", j);
         }
         printf("\n");
     }
     return 0;
 }
-
