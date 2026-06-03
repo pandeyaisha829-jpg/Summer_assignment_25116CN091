5ef6eb1fe5835a2162b028f403e7ab4690991ca4
Aisha <pandeyaisha829@gmail.com>
2026-06-03 13:33:41 +0530

    4th program of Day-9

---
+++ Day-9/program4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+//print reverse star pattern
+int main () {
+    int row,i,j;
+    printf("enter the number of rows");
+    scanf("%d",&row);
+    for(i=row; i>=1; i--){
+        for(j=1; j<=row; j++){
+            if(i==1 || i==row || j==1 || j==row){
+            printf("*");
+        }
+        else{
+        printf(" ");
+    }
+}
+printf("\n");
+    }
+    return 0;
+}
+
