9af6cf6fe1cbeec5b195fff950677aff881dd84f
Aisha <pandeyaisha829@gmail.com>
2026-06-10 10:40:56 +0530

    reupload first program of Day-10

--- Day-10/program1.c
+++ Day-10/program1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 //print  star pyramid  pattern
 int main () {
-    int row,i,j,k;
+    int rows,i,j,k;
     printf("enter the number of rows");
-    scanf("%d",&row);
-    for(i=0; i<row; i++){
-        for(j=0; j<row-i-1; j++){
+    scanf("%d",&rows);
+    for(i=0; i<rows; i++){
+        for(j=0; j<rows-i-1; j++){
             printf(" ");
         }
         for(k=0; k<(2*i+1); k++){
